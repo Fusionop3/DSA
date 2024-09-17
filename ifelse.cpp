7b05f012891704ec6ba335cf1824f15d34882b80
Fusionop3 <vineetkumar2823@outlook.com>
2024-09-17 19:45:39 +0530

    Initial commit

---
+++ ifelse.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num=1;
+    if(num<=3){
+        cout<<"It is small";
+    }else{
+        cout<<"No it is not small";
+    }
+
+
+    return 0;
+}
