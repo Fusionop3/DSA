c84ad0eb77b868703e35eb7a9086f3ffe0b051d1
Fusionop3 <vineetkumar2823@outlook.com>
2024-09-17 21:14:03 +0530

    commited

---
+++ elseif.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"Enter the number:"3
+    ;
+    cin>>a;
+    if(a<=3){
+        cout<<"Yes it is small.";
+    }else{
+        cout<<"No it is not small.";
+    }
+    return 0;
+}
