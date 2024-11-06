0ef8a5da17ab3bf440e6585c81a040b74a63c71a
Fusionop3 <vineetkumar2823@outlook.com>
2024-11-06 11:23:34 +0530

    commit

---
+++ function1.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+    void add(int a , int b){
+        int sum= a+b;
+        cout<<sum;
+    }
+int main()
+{
+    int a = 3;
+    int b = 3;
+
+    add(a,b);
+    return 0;
+
+}
+    
